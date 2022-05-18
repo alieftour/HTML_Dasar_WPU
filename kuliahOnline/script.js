bd5584eff9eb3c8dcddb0f295b216e3daf929de8
alieftour <alieftour@gmail.com>
2022-05-18 08:01:20 +0700

    Menyelesaikan latihan juragan angkot pengulangan dengan while

--- kuliahOnline/script.js
+++ kuliahOnline/script.js
@@ -1,1 +1,30 @@
 // Control Flow membutuhkan Pengulangan & Pengkondisian
+
+// Pengulangan while pada JS
+// while (kondisi) {aksi}
+
+/*var ulang = true;
+while (ulang) {
+  console.log('Hello World!');
+  ulang = confirm('lagi?');
+} */ // berhenti oleh user 
+
+/*
+var nilaiAwal=1;
+while (nilaiAwal<=5) {
+  console.log('Hello World!');
+  nilaiAwal++;
+}*/ // berhenti oleh program
+
+/* var nilaiAwal=1;
+while (nilaiAwal<=5) {
+  console.log('Hello World ' + nilaiAwal + 'x');
+  nilaiAwal++;
+}*/
+
+// Juragan Angkot 1
+var noAngkot=1;
+while(noAngkot<=10) {
+  console.log('Angkot No. ' +noAngkot+ ' beroperasi dengan baik.');
+  noAngkot++;
+}
